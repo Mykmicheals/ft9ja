f9debe6d3b97b75cd3fc80387dce712fdf8e5904
Andrew Micheal <mykmicheals@gmail.com>
2023-05-21 15:32:06 +0100

    added signup view

--- userauth/views.py
+++ userauth/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import SignupSerializer
+from django.contrib.auth.models import User
+
+
+class SignupView(APIView):
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = User.objects.create_user(
+                username=serializer.validated_data['username'],
+                password=serializer.validated_data['password'],
+                email=serializer.validated_data['email']
+            )
+            return Response({'message': 'Signup successful'}, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
